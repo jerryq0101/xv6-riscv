4dafd85c596728e307e208aa2780b9195aca10e0
Jerry Qi <jerryq0101@gmail.com>
2025-02-19 16:30:16 -0500

    with ticks

--- user/hello.c
+++ user/hello.c
@@ -3,12 +3,15 @@
 
 #define PGSIZE 4096
 
-void print_memory_statistics(struct memstat *s, int i);
+void print_memory_statistics(struct memstat *s, int i, int start_time);
 
 int main(int argc, char *argv[])
 {
   printf("Hello my name is %s\n", argv[0]);
 
+        // Get starting time in ticks
+        int start_time = uptime();
+        
         // Lazy allocating user memory (In this implementation)
         int *arr = malloc(PGSIZE * 1000 * sizeof(int));
         struct memstat *s = malloc(sizeof(struct memstat));
@@ -16,7 +19,7 @@ int main(int argc, char *argv[])
         for (int i = 0; i < PGSIZE * 1000; i+=PGSIZE)
         {
                 arr[i] += i;
-                print_memory_statistics(s, i);
+                print_memory_statistics(s, i, start_time);
         }
 
         int m = fork();
@@ -24,20 +27,29 @@ int main(int argc, char *argv[])
         {
                 malloc(PGSIZE * 8);
                 getmemstat(s);
-                printf("index (in child process): pages_curr_allocated: %ld, total_allocations: %ld\n", s->total_allocated_pages, s->total_allocations);
+                printf("Child at time %d: pages_curr_allocated: %ld, total_allocations: %ld\n", 
+                       uptime() - start_time, 
+                       s->total_allocated_pages, 
+                       s->total_allocations);
                 exit(0);
         }
         int thing;
         wait(&thing);
         getmemstat(s);
-        printf("index (AFTER RUN): pages_curr_allocated: %ld, total_allocations: %ld\n", s->total_allocated_pages, s->total_allocations);
+        printf("AFTER RUN at time %d: pages_curr_allocated: %ld, total_allocations: %ld\n", 
+               uptime() - start_time,
+               s->total_allocated_pages, 
+               s->total_allocations);
 
         exit(0);
 }
 
-
-void print_memory_statistics(struct memstat *s, int i)
+void print_memory_statistics(struct memstat *s, int i, int start_time)
 {
         getmemstat(s);
-        printf("index %d: pages_curr_allocated: %ld, total_allocations: %ld\n", i, s->total_allocated_pages, s->total_allocations);
+        printf("time %d, index %d: pages_curr_allocated: %ld, total_allocations: %ld\n", 
+               uptime() - start_time,
+               i, 
+               s->total_allocated_pages, 
+               s->total_allocations);
 }
